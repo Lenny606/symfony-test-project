fe97648394bc971ee34eb78f7f088e9d30b4110c
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-04-24 23:10:35 +0200

    fix commit issue

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,24 +2,61 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController
+class HomePageController extends AbstractController
 {
 
-    #[Route("/", name: "app.index")]
-    public function index() : Response
+    #[Route("/{limit<\d+>?0}", name: "app.index")]
+    public function index(int $limit): Response
     {
-        return new Response("hello");
+
+
+        $template = $this->render(
+            'homepage/homepage.html.twig',
+            [
+                'limit' => $limit,
+                'id' => $limit,
+                'pageTitle' => 'Homepage',
+                'featuredProducts' => [
+                    [
+                        'name' => "Adui",
+                        'description' => "skkvnsen",
+                        'price' => "20"
+                    ],
+                   [
+                       'name' => "AFe",
+                       'description' => "acdc",
+                       'price' => "220"
+                   ],
+                    [
+                        'name' => "",
+                        'description' => "acdc",
+                        'price' => "220"
+                    ]
+                ],
+                'currentYear' => date('Y'),
+            ]
+        );
+
+        return $template;
     }
 
-    #[Route("/show/{id<\d>}", name: "app.show")]
-    public function show(int $id) : Response
+    #[Route("/show/{id<\d>}", name: "app.show", /* requirements: ["id" => '\d+']*/)]
+    public function show(?int $id): Response
     {
-        return new Response("hello id: $id");
-    }
+        $val = $id === null ? $id : '';
+        $template = $this->render(
+            'show/show.html.twig',
+            [
+                'id' => $id
+            ]
+        );
 
+        return $template;
+    }
 
 
 }
