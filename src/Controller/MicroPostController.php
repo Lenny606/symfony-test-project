5cce1f6cc15569bdabcc1e3b5c98fd5faef5c178
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-04-27 13:48:21 +0200

    micropost working

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(MicroPostRepository $posts, EntityManager $em): Response
+    public function index(MicroPostRepository $posts, EntityManagerInterface $em): Response
     {
 
 //        dd($posts->findAll());
@@ -25,15 +25,14 @@ class MicroPostController extends AbstractController
 
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
-            'post' => $posts->findAll(),
+            'posts' => $posts->findAll(),
         ]);
     }
 
     #[Route('/micro-post/{id<\d+>}', name: 'app_micro_post_show')]
     public function showOne(MicroPost $id) :Response
     {
-        //sensio test
-        dd($id);
+
         return $this->render('micro_post/show.html.twig', [
             'controller_name' => 'MicroPostController',
             'post' => $id
